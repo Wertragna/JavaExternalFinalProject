36b9e2987a5d8732696d42d7590e1e6738cea30f
Bohdana Sherstyniuk <ms.sherstynyuk@gmail.com>
2018-12-18 01:25:23 +0200

    Restricting access to data: Security filter (admin)

---
+++ src/main/java/com/javacourse/security/AuthAdminFilter.java
@@ -0,0 +1,39 @@
+package com.javacourse.security;
+
+import com.javacourse.user.User;
+import com.javacourse.user.role.Role;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+import static java.util.Objects.nonNull;
+
+@WebFilter(urlPatterns = {"/admin/*","/admin"})
+public class AuthAdminFilter extends HttpFilter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(final ServletRequest request,
+                         final ServletResponse response,
+                         final FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpSession session = ((HttpServletRequest) request).getSession();
+        User user = (User) session.getAttribute("user");
+        if (nonNull(session) && nonNull(user)&&(user.getRole().equals(Role.ADMIN)))
+                filterChain.doFilter(request, response);
+         else {
+            RequestDispatcher dispatcher = ((HttpServletRequest) request).getSession().getServletContext().getRequestDispatcher("/error404.jsp"); // вызов страницы ответа на запрос
+            dispatcher.forward(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
